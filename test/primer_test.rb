a83462aede5c3abef96d69b33ee2540a735c53ff
Nathaniel Watts <reg@nathanielwatts.com>
2013-05-22 10:30:09 -0500

    add main error given from tests, hopefully to sort out soon

--- test/primer_test.rb
+++ test/primer_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-describe "" do
+describe Primer do # test/primer_test.rb:3:in `<main>': uninitialized constant Primer (NameError)
   it "should have an initial value in the primes array" do
     @primer = Primer.new
     @primer.num.must_equal 3
