1c081bbcd17254f11aa161a7124d7628a44892a6
Nathaniel Watts <reg@nathanielwatts.com>
2013-05-22 09:41:01 -0500

    add a test helper, including in primer_test

--- test/primer_test.rb
+++ test/primer_test.rb
@@ -1,12 +1,8 @@
-require "minitest/autorun"
+require 'test_helper'
 
-class PrimerTest < Minitest::Unit::Test
-
-  describe "when initialized" do
-    it "should have an initial value in the primes array" do
-      @primer = Primer.new
-      @primer.num.must_equal 3
-    end
+describe "" do
+  it "should have an initial value in the primes array" do
+    @primer = Primer.new
+    @primer.num.must_equal 3
   end
-
 end
