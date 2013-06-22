b7083d1f121be017259a13dbe970858bbab114af
Nathaniel Watts <reg@nathanielwatts.com>
2013-06-21 21:13:02 -0500

    add test to make sure that primes can be recalculated

--- test/calculation_test.rb
+++ test/calculation_test.rb
@@ -33,6 +33,18 @@ describe Primer::Calculation do
     @primer.primes.last.must_equal 2
   end
 
+  # this is to test and make sure that after finding one solution,
+  # the user can find another. originally, this was an issue.
+  it "should properly calculate more than one prime if user continues" do
+    desired_prime1 = "250"
+    desired_prime2 = "10"
+    @primer.run(desired_prime1)
+    solution1 = @primer.primes.last
+    @primer.run(desired_prime2)
+    solution2 = @primer.primes.last
+    wont_equal solution2, solution1
+  end
+
   it "should correctly solve Euler #7 " do
     skip
     desired_prime = "10001"
